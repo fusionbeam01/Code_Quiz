bd22306a35b1cf5273e642f47a79f0407d71a5ac
Kyle Mularoni <kylemularoni@gmail.com>
2021-06-27 14:13:10 -0400

    First commit

---
+++ assets/scripts/questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",
+    },
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",
+    },
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",  
+    },
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",
+    },
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",
+    },
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",
+    },
+    {
+        title: "Place holder text replace later with question",
+        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        answer: "choice 1",
+    }
+
+]
