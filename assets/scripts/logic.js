b4b53698b378c90dcfd4d22aa99e62237588bb6c
Kyle Mularoni <kylemularoni@gmail.com>
2021-06-29 10:13:23 -0400

    Edits

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,39 +1,46 @@
 var time = questions.length *15;
-var currentquestionIndex = 0;
+var currentQuestionIndex = 0;
 
 var startBtn = document.querySelector("#start");
 var questionsElements = document.querySelector("#questions");
-var timmerElemets = document.querySelector("#time");
+var timerElement = document.querySelector("#time");
 var questionChoices = document.querySelector("#choices");
 var startScreen = document.querySelector("#start-screen");
 var titleElement = document.querySelector("#question-title");
 
-
-function startQuiz() {
-    startScreen.setAttribute("class", "hide");
-    questionsElements.removeAttribute("class");
-}
-
-function getCurrentQuestion(){
-    var currentQuestion = questions[currentquestionIndex];
-    titleElement.textContent =currentQuestion.title;
-    questionChoices.textContent = currentQuestion.choice;
+function displayQuestion(){
+    var currentQuestion = questions[currentQuestionIndex];
+    titleElement.textContent = currentQuestion.title;
+//questionChoices.textContent = currentQuestion.choice;
 
     for (var i = 0; i < currentQuestion.choice.length; i++) {
         var choiceNode = document.createElement("button");
-        choiceNode.setAttribute("class", "choices");
+        choiceNode.classList.add("choices");
         choiceNode.setAttribute("value", currentQuestion.choice[i]);
-
         choiceNode.textContent = i + 1 + ". " + currentQuestion.choice[i];
+        choiceNode.addEventListener('click', function(event) {
+            if(event.target.value === currentQuestion.answer){
+               //GOT ANSWER RIGHT
+               currentQuestionIndex++
+               displayQuestion()
+
+            } else {
+                //GOT IT WRONG
+                time -= 5
+                
+            }
+        })
+
 
         questionChoices.appendChild(choiceNode);
     }
 }
 
-
-
-
-
+function startQuiz() {
+    startScreen.classList.toggle("hide");
+    questionsElements.classList.toggle('hide')
+    displayQuestion()
+}
 
 
 startBtn.addEventListener("click", startQuiz);
--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -1,14 +1,14 @@
 // REMINDER CHAGE THE QUESIONS FROM PLACE HOLDER BEFORE SUBMISSION OF CODE
 var questions = [
     {
-        title: "Place holder text replace later with question",
-        choice: ["choice 1", "choice 2","choice 3","choice 4"],
+        title: "Which of the following is a tratior legion",
+        choice: ["Blood Angles", "I","choice 3","choice 4"],
         answer: "choice 1",
     },
     {
-        title: "Place holder text replace later with question",
-        choice: ["choice 1", "choice 2","choice 3","choice 4"],
-        answer: "choice 1",
+        title: "Do I like JavaScript",
+        choice: ["Hell Yeah", "My porridge is too cold","It's great","Yes I do"],
+        answer: "Hell Yeah",
     },
     {
         title: "Place holder text replace later with question",
@@ -38,4 +38,3 @@ var questions = [
 
 ];
 
-console.log(questions.choice[0]);
